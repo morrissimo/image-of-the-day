c043d5871310008aa9e5cdb6620ede472b25a053
Robert Morris <robert.f.morris@gmail.com>
2015-02-25 10:18:46 -0500

    db-related tweaks to settings

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -59,15 +59,20 @@ WSGI_APPLICATION = 'iotd.wsgi.application'
 
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
-       'default': {
-             'ENGINE':'django.db.backends.postgresql_psycopg2',
-             'NAME': os.environ['RDS_DB_NAME'],
-             'USER': os.environ['RDS_USERNAME'],
-             'PASSWORD': os.environ['RDS_PASSWORD'],
-             'HOST': os.environ['RDS_HOSTNAME'],
-             'PORT': os.environ['RDS_PORT'],
-             }
-          }
+        'default': {
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            # iotddev
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            # iotddev12!
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+            'OPTIONS': {
+                'init_command': 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',   # MySQL uses REPEATABLE READ by default
+            },
+        }
+    }
 else:
     DATABASES = {
         'default': {
